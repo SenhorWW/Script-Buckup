3d0305ec91ec5d2c29a42b788bb438fcad4e047f
WALLACE WILLIAM FARIAS DOS SANTOS <wallace.wfsantos@senacsp.edu.br>
2024-10-21 16:21:19 -0300

    V. 1.0

---
+++ backup.bat
@@ -0,0 +1,5 @@
+mkdir Backup
+copy /windows/*.txt \Users\wallace.wfsantos\Documents\script-backup 
+copy /windows/System32/*.log \Users\wallace.wfsantos\Documents\script-backup 
+copy /windows/System32/*.txt \Users\wallace.wfsantos\Documents\script-backup 
+
